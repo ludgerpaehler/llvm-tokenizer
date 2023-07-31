9ff34700cfc15f5a4fdf86ccbf27db03d84c708f
Aiden Grossman <agrossman154@yahoo.com>
2023-07-31 16:59:46 -0700

    clang-format llvm-tokenizer.cpp

--- llvm-tokenizer.cpp
+++ llvm-tokenizer.cpp
@@ -1,8 +1,8 @@
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
 #include <llvm/IRReader/IRReader.h>
-#include <llvm/Support/MemoryBuffer.h>
 #include <llvm/Support/Error.h>
+#include <llvm/Support/MemoryBuffer.h>
 #include <llvm/Support/SourceMgr.h>
 
 #include <iostream>
@@ -14,17 +14,19 @@ static ExitOnError ExitOnErr("llvm-tokenizer error: ");
 int main() {
   LLVMContext Context;
 
-  std::unique_ptr<llvm::MemoryBuffer> FileBuffer = ExitOnErr(errorOrToExpected(MemoryBuffer::getFile("./test.ll")));
+  std::unique_ptr<llvm::MemoryBuffer> FileBuffer =
+      ExitOnErr(errorOrToExpected(MemoryBuffer::getFile("./test.ll")));
 
   SMDiagnostic ParseError;
-  std::unique_ptr<llvm::Module> IRModule = parseIR(*FileBuffer, ParseError, Context);
+  std::unique_ptr<llvm::Module> IRModule =
+      parseIR(*FileBuffer, ParseError, Context);
 
-  if(!IRModule) {
+  if (!IRModule) {
     ParseError.print("./test.ll", errs());
     return 1;
   }
 
-  for(Function &IRFunction : *IRModule) {
+  for (Function &IRFunction : *IRModule) {
     std::cout << IRFunction.getName().str() << "\n";
   }
 
